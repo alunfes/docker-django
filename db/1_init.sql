fedfb9d9984c7b2fcfb1f45220bb6bcaee1176f2
alunfes <ts4792@gmail.com>
2018-12-01 22:38:53 +0900

    1st Dec 2018

--- db/1_init.sql
+++ db/1_init.sql
@@ -1,3 +1,3 @@
-create role user1 login password 'user1';
-create database testdb;
-grant all privileges on database user1 to user1;
+CREATE USER user1 PASSWORD 'user1';
+CREATE DATABASE testdb;
+GRANT all privileges on database user1 to user1;
